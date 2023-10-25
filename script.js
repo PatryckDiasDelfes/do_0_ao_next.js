f6b98ec4384974f4bd57c782fb5ab3b470410dca
Patryck Dias Delfes <delfesne@gmail.com>
2023-10-25 12:34:47 -0300

    Finalização e explicação

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ function mark(indice) {
     if (jogador == "X") {
         jogador = "O"
     } 
-//Vefica(==) se é "X". Se for "X" ele atribui(=) valor para "0"
+//Vefica(==) se é "jogador= X". Se for " jogador= X" ele atribui(=) valor para "jogador= O"
 
     else {
         jogador = "X"
     }
-//Se valor foi verificado como "0", ele reatribui valor para "X"
+//Se valor foi verificado como "jogador= O", ele reatribui valor para "jogador= X"
 
     render()
-//Antes do render atualizamos o estado da aplicação. Agora chamando o "render" atualizamos a tela do user.
+//Antes do render, atualizamos o estado da aplicação, para agora chamar o "render" atualizamos a tela do user.
 }
 
 function render() {
     let tds = document.querySelectorAll("td")
-//"let tds = document.querySelectorAll("td") acessa o documento do aplicação e verifica os "td" 
+//"let tds = document.querySelectorAll("td") acessa o documento do aplicação e seleciona os "td" 
     tds.forEach((td, indice) => {
         td.innerHTML = table[indice]
     })
-//Chamando a variavel tds ele verifica cada uma(forEach) e da um valor(td) e verifica o valor de "indice". 
-//Para então verificar(innerHTML) oque tem em "td" e atribuir o valor de tabela/indice.
+//Chamando a variavel "tds" ele passa por cada uma(forEach) e então executa um bloco de codigo "{}".
+//Para então acessar o "innerHTML". Pegar oque tem em "td" e atribuir o valor de tabela/indice.
 //Por fim ele atualiza a tela 
 }
